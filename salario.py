d60291fee7f419b079282cd9817d2cd9f0402565
Paulo Henrique Silva de Andrade <yogscastfriend@gmail.com>
2026-09-09 14:11:02 -0300

    salario.py

---
+++ salario.py
@@ -0,0 +1,6 @@
+valor_hora = float(input("Digite o valor da hora trabalhada:"))
+total_hora = float(input("Digite o total de horas trabalhadas: "))
+
+salario = valor_hora * total_hora
+
+print(f"Seu salário do mês é: R$ {salario:.2f}")
